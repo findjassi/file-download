89917bb8d69b611a2bb2ed8516295072ac634361
Jaspreet Arora <findjassi@gmail.com>
2024-10-22 09:47:36 +0100

    chore: Added structure for components

---
+++ src/components/FileDownloadComponent/FileDownloadComponet.tsx
@@ -0,0 +1,18 @@
+import ListHeader from "../common/ListHeader/ListHeader";
+import Table from "../common/Table/Table";
+
+const FileDownloadComponet = () => {
+  return (
+    <div>
+      <h1>File Download Component</h1>
+
+      {/* ListHeader */}
+      <ListHeader />
+
+      {/* Table Component */}
+      <Table />
+    </div>
+  );
+};
+
+export default FileDownloadComponet;
---
+++ src/components/common/ListHeader/ListHeader.tsx
@@ -0,0 +1,9 @@
+const ListHeader = () => {
+  return (
+    <div>
+      <h1>List Header</h1>
+    </div>
+  );
+};
+
+export default ListHeader;
